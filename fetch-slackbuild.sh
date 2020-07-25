13afbe81d338dc71515aa1b36272237a14968e5b
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-07-25 21:42:36 +0300

    Make compatible with bash, dash, ksh, zsh, ash
    Sorry, csh. 😢

--- fetch-slackbuild.sh
+++ fetch-slackbuild.sh
@@ -3,10 +3,13 @@
 # Get OS release information
 . /etc/*-release
 
+# This is the directory where all the downloaded files will be downloaded
+DOWNLOADS_DIR=/tmp/slackbuilds
+
+# The base URL where the SlackBuild files will be downloaded from
 SLACKBUILDS_URL=https://slackbuilds.org/slackbuilds/$VERSION_ID
-SLACKBUILDS_DIR=/tmp/slackbuilds
-SLACKBUILDS_TXT_URL=$SLACKBUILDS_URL/SLACKBUILDS.TXT.gz
-SLACKBUILDS_TXT=$SLACKBUILDS_DIR/$(basename $SLACKBUILDS_TXT_URL)
+
+# This is the package name given as an argument
 PACKAGE=$*
 
 # Make sure we're running on Slackware
@@ -21,26 +24,27 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-# Make sure the temporary directory exists
-mkdir -p $SLACKBUILDS_DIR
-pushd $SLACKBUILDS_DIR > /dev/null
+# Make sure the downloads directory exists
+mkdir -p $DOWNLOADS_DIR
 
 # Download SLACKBUILDS.TXT
-echo -e "Updating SlackBuild list from $SLACKBUILDS_TXT_URL..."
-wget -qN $SLACKBUILDS_TXT_URL
+SLACKBUILDS_TXT_URL=$SLACKBUILDS_URL/SLACKBUILDS.TXT.gz
+SLACKBUILDS_TXT=$DOWNLOADS_DIR/$(basename $SLACKBUILDS_TXT_URL)
+echo "Updating SlackBuild list from $SLACKBUILDS_TXT_URL..."
 
-if ! [ $? = 0 ]; then
+if ! wget -qNP $DOWNLOADS_DIR $SLACKBUILDS_TXT_URL; then
   echo "Failed to download list of SlackBuilds; aborting."
   exit 1
 fi
 
+echo "Finding package '$PACKAGE'..."
+
 # Get package metadata
-METADATA=$(zcat $SLACKBUILDS_TXT | \
-  grep -e "^SLACKBUILD NAME: $PACKAGE$" -A 9 -m 1)
+METADATA=$(zgrep -e "^SLACKBUILD NAME: $PACKAGE$" -A 9 -m 1 $SLACKBUILDS_TXT)
 
 # Make sure the package exists
-if [ -z "$METADATA" ]; then
-  echo "Package '$PACKAGE' not found in $SLACKBUILDS_TXT; aborting."
+if ! [ $? = 0 ]; then
+  echo "Package '$PACKAGE' not found; aborting."
   exit 1
 fi
 
@@ -50,7 +54,6 @@ read_metadata_field() {
 }
 
 SLACKBUILD_NAME=$(read_metadata_field NAME)
-SLACKBUILD_REQUIRES=$(read_metadata_field REQUIRES)
 SLACKBUILD_DOWNLOAD_x86_64=$(read_metadata_field DOWNLOAD_x86_64)
 
 # Get the correct download link and MD5 sum based on architecture
@@ -70,37 +73,32 @@ fi
 
 # Download the SlackBuild
 SLACKBUILD_URL="$SLACKBUILDS_URL/$(read_metadata_field LOCATION).tar.gz"
-SLACKBUILD_TARBALL="$SLACKBUILDS_DIR/$(basename $SLACKBUILD_URL)"
-echo -e "Downloading SlackBuild from $SLACKBUILD_URL..."
-wget -qN $SLACKBUILD_URL
-if ! [ $? = 0 ]; then
+SLACKBUILD_TARBALL="$DOWNLOADS_DIR/$(basename $SLACKBUILD_URL)"
+echo "Downloading SlackBuild from $SLACKBUILD_URL..."
+if ! wget -qNP $DOWNLOADS_DIR $SLACKBUILD_URL; then
   echo "Failed to download SlackBuild; aborting."
   exit 1
 fi
 
 # Extract the SlackBuild
-echo -e "Extracting SlackBuild tarball $SLACKBUILD_TARBALL..."
-tar -xf $SLACKBUILD_TARBALL -C $SLACKBUILDS_DIR
-if ! [ $? = 0 ]; then
+echo "Extracting SlackBuild tarball $SLACKBUILD_TARBALL..."
+if ! tar -xf $SLACKBUILD_TARBALL -C $DOWNLOADS_DIR; then
   echo "Failed to extract SlackBuild tarball; aborting."
   exit 1
 fi
 
 # Download the upstream source tarball
-SOURCE_TARBALL="$SLACKBUILDS_DIR/$SLACKBUILD_NAME/$(basename $SLACKBUILD_DOWNLOAD)"
-pushd $(dirname $SOURCE_TARBALL) > /dev/null
-echo -e "Downloading source archive from $SLACKBUILD_DOWNLOAD..."
-wget -qN $SLACKBUILD_DOWNLOAD
-if ! [ $? = 0 ]; then
+SOURCE_TARBALL="$DOWNLOADS_DIR/$SLACKBUILD_NAME/$(basename $SLACKBUILD_DOWNLOAD)"
+#pushd $(dirname $SOURCE_TARBALL) > /dev/null
+echo "Downloading source archive from $SLACKBUILD_DOWNLOAD..."
+if ! wget -qNP $(dirname $SOURCE_TARBALL) $SLACKBUILD_DOWNLOAD; then
   echo "Failed to download source archive; aborting."
   exit 1
 fi
-popd > /dev/null
 
 # Check the MD5 sum of the downloaded source against the one in the SlackBuild
 echo "Checking MD5 sum against the one provided with SlackBuild..."
-md5sum --quiet -c <<< "$SLACKBUILD_MD5SUM $SOURCE_TARBALL"
-if ! [ $? = 0 ]; then
+if ! echo "$SLACKBUILD_MD5SUM $SOURCE_TARBALL" | md5sum --quiet -c; then
   echo "MD5 sums do not match; aborting."
   exit 1
 fi
@@ -108,19 +106,17 @@ fi
 # Print a short summary
 echo "All done! To create your Slackware package, execute:"
 echo
-echo "  cd $SLACKBUILDS_DIR/$SLACKBUILD_NAME && ./$SLACKBUILD_NAME.SlackBuild"
+echo "  cd $DOWNLOADS_DIR/$SLACKBUILD_NAME && ./$SLACKBUILD_NAME.SlackBuild"
 echo
 
 # Search for dependencies and make a list of missing ones, for convenience
+SLACKBUILD_REQUIRES=$(read_metadata_field REQUIRES)
 MISSING_DEPS=
 for dep in $SLACKBUILD_REQUIRES; do
   if [ "$(ls /var/log/packages | grep -e '^$dep-')" = "" ]; then
-    MISSING_DEPS="$MISSING_DEPS$dep "
+    MISSING_DEPS="$MISSING_DEPS $dep"
   fi
 done
 if ! [ -z "$MISSING_DEPS" ]; then
   echo "These dependencies have to be installed: $MISSING_DEPS"
 fi
-
-popd > /dev/null
-
