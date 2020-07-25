4a60b157e2588a515ce1902923332f75acccea67
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-07-25 13:27:30 +0300

    Make the script more robust
    Added architecture detection to avoid running e.g. on ARM.
    We don't give an output filename for wget anymore, making
    it possible for wget to only download the file if it has changed.
    Changed all backticks to dollar syntax, and other small
    optimizations.

--- fetch-slackbuild.sh
+++ fetch-slackbuild.sh
@@ -6,18 +6,30 @@
 SLACKBUILDS_URL=https://slackbuilds.org/slackbuilds/$VERSION_ID
 SLACKBUILDS_DIR=/tmp/slackbuilds
 SLACKBUILDS_TXT_URL=$SLACKBUILDS_URL/SLACKBUILDS.TXT.gz
-SLACKBUILDS_TXT=$SLACKBUILDS_DIR/`basename $SLACKBUILDS_TXT_URL`
+SLACKBUILDS_TXT=$SLACKBUILDS_DIR/$(basename $SLACKBUILDS_TXT_URL)
 INSTALLED_PACKAGES=/var/log/packages
 PACKAGE=$*
-
-if [ $# -eq 0 ]; then
-  echo "No package name given."
-  exit 1
-fi
 
 # Make sure we're running on Slackware
 if ! [ "$NAME" = "Slackware" ]; then
   echo "Not running on Slackware; aborting."
+  exit 1
+fi
+
+# Make sure we're running on a supported architecture
+MARCH=$(uname -m)
+case "$MARCH" in
+  i?86)   ;;
+  x86_64) ;;
+  *)      echo "Not running on a supported architecture."
+          echo "Only x86 and x86_64 are supported; you are running on $MARCH."
+          exit 1
+          ;;
+esac
+
+# Show help if no arguments are given
+if [ $# -eq 0 ]; then
+  echo "usage: $0 PACKAGE"
   exit 1
 fi
 
@@ -35,8 +47,8 @@ if ! [ $? = 0 ]; then
 fi
 
 # Get package metadata
-METADATA=`zcat $SLACKBUILDS_TXT | \
-  grep -e "^SLACKBUILD NAME: $PACKAGE$" -A 9 -m 1`
+METADATA=$(zcat $SLACKBUILDS_TXT | \
+  grep -e "^SLACKBUILD NAME: $PACKAGE$" -A 9 -m 1)
 
 # Make sure the package exists
 if [ -z "$METADATA" ]; then
@@ -44,27 +56,27 @@ if [ -z "$METADATA" ]; then
   exit 1
 fi
 
-# Parses the given field from the metadata
-parse_field() {
-  echo `echo "$METADATA" | sed -n -e "s/^SLACKBUILD $*: \(.\+\)$/\1/p"`
+# Reads the given field from the metadata
+read_metadata_field() {
+  echo $(echo "$METADATA" | sed -n -e "s/^SLACKBUILD $*: \(.\+\)$/\1/p")
 }
 
-SLACKBUILD_NAME=`parse_field NAME`
-SLACKBUILD_DOWNLOAD=`parse_field DOWNLOAD`
-SLACKBUILD_DOWNLOAD_x86_64=`parse_field DOWNLOAD_x86_64`
-SLACKBUILD_MD5SUM=`parse_field MD5SUM`
-SLACKBUILD_MD5SUM_x86_64=`parse_field MD5SUM_x86_64`
-SLACKBUILD_REQUIRES=`parse_field REQUIRES`
+SLACKBUILD_NAME=$(read_metadata_field NAME)
+SLACKBUILD_REQUIRES=$(read_metadata_field REQUIRES)
+SLACKBUILD_DOWNLOAD_x86_64=$(read_metadata_field DOWNLOAD_x86_64)
 
 # Get the correct download link and MD5 sum based on architecture
-if [ `uname -m` = "x86_64" ] && ! [ -z $SLACKBUILD_DOWNLOAD_x86_64 ]; then
+if [ $(uname -m) = "x86_64" ] && ! [ -z $SLACKBUILD_DOWNLOAD_x86_64 ]; then
   SLACKBUILD_DOWNLOAD=$SLACKBUILD_DOWNLOAD_x86_64
-  SLACKBUILD_MD5SUM=$SLACKBUILD_MD5SUM_x86_64
+  SLACKBUILD_MD5SUM=$(read_metadata_field MD5SUM_x86_64)
+else
+  SLACKBUILD_DOWNLOAD=$(read_metadata_field DOWNLOAD)
+  SLACKBUILD_MD5SUM=$(read_metadata_field MD5SUM)
 fi
 
 # Download the SlackBuild
-SLACKBUILD_URL="$SLACKBUILDS_URL/`parse_field LOCATION`.tar.gz"
-SLACKBUILD_TARBALL="$SLACKBUILDS_DIR/`basename $SLACKBUILD_URL`"
+SLACKBUILD_URL="$SLACKBUILDS_URL/$(read_metadata_field LOCATION).tar.gz"
+SLACKBUILD_TARBALL="$SLACKBUILDS_DIR/$(basename $SLACKBUILD_URL)"
 echo -e "Downloading SlackBuild from '$SLACKBUILD_URL'...\n"
 wget -N $SLACKBUILD_URL
 if ! [ $? = 0 ]; then
@@ -76,33 +88,29 @@ fi
 echo -e "Extracting SlackBuild tarball '$SLACKBUILD_TARBALL'...\n"
 tar xvf $SLACKBUILD_TARBALL -C $SLACKBUILDS_DIR
 if ! [ $? = 0 ]; then
-  echo "Failed to extract SlackBuild; aborting."
+  echo "Failed to extract SlackBuild tarball; aborting."
   exit 1
 fi
 
 # Download the upstream source tarball
-SOURCE_TARBALL="$SLACKBUILDS_DIR/$SLACKBUILD_NAME/`basename $SLACKBUILD_DOWNLOAD`"
+SOURCE_TARBALL="$SLACKBUILDS_DIR/$SLACKBUILD_NAME/$(basename $SLACKBUILD_DOWNLOAD)"
+pushd $(dirname $SOURCE_TARBALL) > /dev/null
 echo
-echo -e "Downloading source from '$SLACKBUILD_DOWNLOAD'...\n"
-wget -N $SLACKBUILD_DOWNLOAD -O $SOURCE_TARBALL
+echo -e "Downloading source archive from '$SLACKBUILD_DOWNLOAD'...\n"
+wget -N $SLACKBUILD_DOWNLOAD
 if ! [ $? = 0 ]; then
-  echo "Failed to download source; aborting."
-  echo "Please make sure that the SlackBuilds.org public key is added to your keyring."
+  echo "Failed to download source archive; aborting."
   exit 1
 fi
+popd > /dev/null
 
 # Check the MD5 sum of the downloaded source against the one in the SlackBuild
 echo "Checking MD5 sum against the one provided with SlackBuild..."
 echo
-md5sum $SOURCE_TARBALL
-echo "$SLACKBUILD_MD5SUM  (SlackBuild)"
-echo
-SOURCE_MD5SUM=`md5sum $SOURCE_TARBALL | cut -f 1 -d " "`
-if ! [ $SOURCE_MD5SUM = $SLACKBUILD_MD5SUM ]; then
+md5sum -c <<< "$SLACKBUILD_MD5SUM $SOURCE_TARBALL"
+if ! [ $? = 0 ]; then
   echo "MD5 sums do not match; aborting."
-  exit 1;
-else
-  echo "MD5 sums match."
+  exit 1
 fi
 
 # Print a short summary
@@ -115,7 +123,7 @@ echo
 # Search for dependencies and make a list of missing ones, for convenience
 MISSING_DEPS=
 for dep in $SLACKBUILD_REQUIRES; do
-  if [ "`ls $INSTALLED_PACKAGES | grep -e '^$dep-'`" = "" ]; then
+  if [ "$(ls $INSTALLED_PACKAGES | grep -e '^$dep-')" = "" ]; then
     MISSING_DEPS="$MISSING_DEPS$dep "
   fi
 done
